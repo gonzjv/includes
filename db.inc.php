5a9bb1fbb5a8919fefcffb9ecab2c1f86ef8363c
gonzjv <gonzjv@ya.ru>
2017-10-19 12:41:25 +0200

    work-2017-10-19

--- db.inc.php
+++ db.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=football', 'all', 'qwe');
+    $pdo = new PDO('mysql:host=localhost;dbname=jv', 'jv_user', '435761');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->exec('SET NAMES "utf8"');
 } catch (PDOException $e) {
